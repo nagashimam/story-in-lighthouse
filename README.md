0396c3f0ea2949e6b48469fb69cf8a7024a78d32
masato <masato.ngsm@gmail.com>
2021-05-23 05:57:45 +0900

    added lighthouse audit

--- README.md
+++ README.md
@@ -19,3 +19,7 @@ This is simple proof of concept. I may publish this repository as npm package af
 You'll see this error at the first time you run this program. This is because chromium isn't installed to the expected path.
 
 Go to node_modules/puppeteer and you'll find .local-chromium or zip file of chromium. Copy the directory or unzip it to the path according to the error message you're seeing.
+
+### Process keeps waiting
+
+You need to run `npx lighthouse 'https://storybookjs.netlify.app/vue-kitchen-sink/iframe.html?id=issues-11839-undefined-boolean--primary&args=disabled:true&viewMode=story' --output=csv --output-path=stdout` and lighthouse will ask you to get permission to send report. Without running this command beforehand, lighthouse will wait for your input forever and never finish its job.
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,21 @@
 import { StoryIframeLoader } from "./story-loader/story-loader";
-import { StorybookConnection, StoriesBrowser } from "storycrawler";
+import { exec } from "child_process";
 (async () => {
   try {
-    console.log("hoge");
-    // const arg = process.argv.slice(2);
-    // if (arg.length < 1) {
-    //   throw new Error(
-    //     "No URL found.\nPlease tell me your Storybook URL\n(e.g: node dist/main.js https://storybookjs.netlify.app/vue-kitchen-sink)"
-    //   );
-    // }
-    // const loader = new StoryLoader(arg[0]);
-    // console.log(process.argv.slice(2));
-    // Connect to the target Storybook server.
-    const storybookUrl = "https://storybookjs.netlify.app/vue-kitchen-sink";
-    const connection = await new StorybookConnection({
-      storybookUrl,
-    }).connect();
-
-    // Launch Puppeteer process to fetch stories info.
-    const storiesBrowser = await new StoriesBrowser(connection).boot();
-    // Item in stories has name, kind and id of the corresponding story
-    const stories = await storiesBrowser.getStories();
-    console.log("found stories", stories);
+    const arg = process.argv.slice(2);
+    if (arg.length < 1) {
+      throw new Error(
+        "No URL found.\nPlease tell me your Storybook URL\n(e.g: node dist/main.js https://storybookjs.netlify.app/vue-kitchen-sink)"
+      );
+    }
+    await exec("echo '' > report.csv");
+    const iframs = await new StoryIframeLoader().getStoryIframeUrls(arg[0]);
+    iframs.forEach(async (iframe) => {
+      console.log(`audit for ${iframe}`);
+      await exec(
+        `npx lighthouse '${iframe}' --output='csv' --output-path='stdout' >> report.csv`
+      );
+    });
   } catch (e) {
     console.log(e);
   }
